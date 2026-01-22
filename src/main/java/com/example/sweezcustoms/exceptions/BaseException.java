e9f9e3162aec7f331a714387b897e8759035a07f
Dautov Muslim <dautov.292009@gmail.com>
2026-01-22 11:46:26 +0600

    FINISHED TO DO EXCEPTIONHANDLER

--- src/main/java/com/example/sweezcustoms/exceptions/BaseException.java
+++ src/main/java/com/example/sweezcustoms/exceptions/BaseException.java
@@ -1,10 +1,14 @@
 package com.example.sweezcustoms.exceptions;
 
+import lombok.Getter;
+
+@Getter
 public class BaseException extends RuntimeException {
+    private final ErrorBody errorBody;
 
 
-
-    public BaseException(String message) {
-        super(message);
+    public BaseException(ErrorBody errorBody) {
+        super(errorBody.getMessage());
+        this.errorBody = errorBody;
     }
 }
--- src/main/java/com/example/sweezcustoms/exceptions/BothFieldNullPointerException.java
+++ src/main/java/com/example/sweezcustoms/exceptions/BothFieldNullPointerException.java
@@ -1,8 +1,10 @@
 package com.example.sweezcustoms.exceptions;
 
 
+import java.util.Date;
+
 public class BothFieldNullPointerException extends BaseException {
     public BothFieldNullPointerException(String message) {
-        super(message);
+        super(ErrorBody.builder().message(message).code(400).date(new Date()).build());
     }
 }
